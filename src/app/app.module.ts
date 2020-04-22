7a24513165e1a0775851d757ce909e2c8c0a8892
TomJelda <61269415+TomJelda@users.noreply.github.com>
2020-04-22 21:11:45 +0200

    oprava

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
-import { HomePageComponent } from './Pages/Home/home-page/home-page.component';
 import { UserPageComponent } from './Pages/User/user-page/user-page.component';
 import { ConfigurationPageComponent } from './Pages/Configurations/configuration-page/configuration-page.component';
-import { BackupsPageComponent } from './Pages/Backups/backups-page/backups-page.component';
 import { Routes } from '@angular/router';
 import { ConfigurationTableComponent } from './Pages/Configurations/configuration-table/configuration-table.component';
 import { IncomingBackupComponent } from './Pages/Backups/incoming-backup/incoming-backup.component';
@@ -15,7 +13,6 @@ import { CompletedBackupComponent } from './Pages/Backups/completed-backups/comp
 import { ClientPageComponent } from './Pages/Clients/client-page/client-page.component';
 import { ClientsTableComponent } from './Pages/Clients/clients/clients.component';
 import { NewClientsComponent } from './Pages/Clients/new-clients/new-clients.component';
-import { ClientsHistoryComponent } from './clients-history/clients-history.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegisterComponent } from './Pages/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -30,17 +27,14 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
     UserPageComponent,
     ConfigurationPageComponent,
-    BackupsPageComponent,
     ConfigurationTableComponent,
     IncomingBackupComponent,
     CompletedBackupComponent,
     ClientPageComponent,
     ClientsTableComponent,
     NewClientsComponent,
-    ClientsHistoryComponent,
     RoutingComponents,
     LoginComponent,
     RegisterComponent,
