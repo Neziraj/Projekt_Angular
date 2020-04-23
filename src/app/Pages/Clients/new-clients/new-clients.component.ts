dfcab7c6b2525904bc517b1b9055a254feef1a91
Tomas <jelinektomas@sssvt.cz>
2020-04-23 22:28:47 +0200

    Funkční dialog!!!!!!!

--- src/app/Pages/Clients/new-clients/new-clients.component.ts
+++ src/app/Pages/Clients/new-clients/new-clients.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {NewClientsQueryService} from '../../../Services/Templates/DataService/Queries/NewClientsQueryService';
 import {NewClientsQuery} from '../../../Models/Queries/NewClientsQuery';
 
+import { ModalService } from 'src/app/_modal';
+
 
 @Component({
   selector: 'app-new-clients',
@@ -10,13 +12,22 @@ import {NewClientsQuery} from '../../../Models/Queries/NewClientsQuery';
 })
 export class NewClientsComponent implements OnInit {
   NewClientsQuery$: NewClientsQuery[];
+  bodyText: string;
 
   headers = ['Datum', 'Název', '', ''];
-  constructor(private dataService: NewClientsQueryService) { }
+  constructor(private dataService: NewClientsQueryService, private modalService: ModalService) { }
 
   ngOnInit(){
+    this.bodyText = 'This text can be updated in modal 1';
     return this.dataService.get()
       .subscribe(data => this.NewClientsQuery$ = data);
   }
 
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }
